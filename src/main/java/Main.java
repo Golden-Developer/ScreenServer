ed0cb47bb05305279b1545496f2d0082eeaaa10d
Sebastian Hobst <hobstsebastian@gmail.com>
2022-08-18 00:13:15 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        JFrame jframe = new JFrame("Server");
+        jframe.setSize(400, 400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JLabel jLableText = new JLabel("Waiting for image from client...");
+
+        jframe.add(jLableText, BorderLayout.SOUTH);
+
+        jframe.setVisible(true);
+
+
+        ServerSocket serverSocket = new ServerSocket(1234);
+
+        Socket socket = serverSocket.accept();
+
+        InputStream inputStream = socket.getInputStream();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+
+        BufferedImage bufferedImage = ImageIO.read(bufferedInputStream);
+
+        bufferedInputStream.close();
+        socket.close();
+
+        JLabel jLablePic = new JLabel(new ImageIcon(bufferedImage));
+        jLableText.setText("image received.");
+        jframe.add(jLablePic, BorderLayout.CENTER);
+
+
+    }
+}
