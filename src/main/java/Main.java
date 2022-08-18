4d212ccb373d2ca57f9cd8b75872ad99b382864b
Collin Ilgner <c.ilgner@tmg-web.de>
2022-08-18 08:07:38 +0200

    Remove: .idea and /target Directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.ServerSocket;
